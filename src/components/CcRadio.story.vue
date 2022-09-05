2c21492ac223112c933132fea69fbaaa5cb340fe
Colin Johnsun <colin.johnsun@gmail.com>
2022-09-05 19:36:28 +1000

    feat: Radio component

---
+++ src/components/CcRadio.story.vue
@@ -0,0 +1,47 @@
+<script setup lang="ts">
+import { ref } from "vue";
+import CcRadio from "./CcRadio.vue";
+
+const name1 = ref("");
+const name2 = ref("");
+const disabled = ref(false);
+const outline = ref(false);
+</script>
+
+<template>
+  <Story title="CcRadio" :layout="{ type: 'grid', width: '100%' }">
+    <Variant title="Options array">
+      <CcRadio
+        v-model="name1"
+        :options="['Tom', 'Peter', 'Mary']"
+        :outline="outline"
+        :disabled="disabled"
+        class="grid"
+      />
+
+      <div>{{ name1 }}</div>
+    </Variant>
+    <Variant title="Options object">
+      <CcRadio
+        v-model="name2"
+        :options="{ tom: 'Tom Smith', pete: 'Peter Jones', mary: 'Mary Thompson' }"
+        :outline="outline"
+        :disabled="disabled"
+        class="grid"
+      />
+
+      <div>{{ name2 }}</div>
+    </Variant>
+    <template #controls>
+      <HstCheckbox v-model="outline" title="Outline" />
+      <HstCheckbox v-model="disabled" title="Disabled" />
+    </template>
+  </Story>
+</template>
+
+<style lang="scss" scoped>
+.grid {
+  display: grid;
+  gap: 8px;
+}
+</style>
---
+++ src/components/CcRadio.vue
@@ -0,0 +1,58 @@
+<script setup lang="ts">
+import { computed, ref, watch } from "vue";
+import { uniqueId } from "@/utils/unique-id";
+
+type RadioOptions = Array<string> | Record<string, string>;
+
+interface Props {
+  modelValue: string | undefined;
+  options: RadioOptions;
+  inline?: boolean;
+  outline?: boolean;
+  disabled?: boolean;
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  inline: false,
+  outline: false,
+  disabled: false,
+});
+const emit = defineEmits<{
+  (e: "update:modelValue", v: string | undefined): void;
+}>();
+
+const id = uniqueId("radio");
+const selected = ref<string>();
+const radioOptions = computed<[string, string][]>(() => {
+  if (Array.isArray(props.options)) {
+    return props.options.map((s) => [s, s]);
+  } else {
+    return Object.entries(props.options);
+  }
+});
+watch(selected, (val) => emit("update:modelValue", val), { immediate: true });
+</script>
+
+<template>
+  <div :class="{ 'cc-radio--disabled': props.disabled }">
+    <div
+      v-for="(option, idx) in radioOptions"
+      :key="option[0]"
+      class="cc-radio"
+      :class="{ 'cc-radio--inline': inline }"
+    >
+      <input
+        :id="`${id}--${idx}`"
+        v-model="selected"
+        type="radio"
+        :name="id"
+        :value="option[0]"
+        :checked="props.modelValue === option[0]"
+        :disabled="props.disabled"
+      />
+      <label :for="`${id}--${idx}`" class="cc-radio__label" :class="{ 'cc-radio--outline': outline }">
+        <slot :option="option">{{ option[1] }}</slot>
+      </label>
+    </div>
+  </div>
+</template>

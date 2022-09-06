259277f415db635ecbda178902bdbc3a2ac1b718
Colin Johnsun <colin.johnsun@gmail.com>
2022-09-06 10:33:45 +1000

    wip: Initial select component

---
+++ src/components/CcSelect.story.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+import { ref } from "vue";
+import CcSelect from "./CcSelect.vue";
+
+const name1 = ref<string[]>([]);
+// const name2 = ref<string[]>([]);
+const disabled = ref(false);
+</script>
+
+<template>
+  <Story title="CcSelect" :layout="{ type: 'grid', width: '100%' }">
+    <Variant title="Options array">
+      <div class="playground">
+        <CcSelect v-model="name1" :options="['Tom', 'Peter', 'Mary']" :disabled="disabled" />
+      </div>
+      <div>{{ name1 }}</div>
+    </Variant>
+
+    <!-- <Variant title="Options object">
+      <CcSelect
+        v-model="name2"
+        :options="{ tom: 'Tom Smith', pete: 'Peter Jones', mary: 'Mary Thompson' }"
+        :disabled="disabled"
+        class="grid"
+      />
+
+      <div>{{ name2 }}</div>
+    </Variant> -->
+
+    <template #controls>
+      <HstCheckbox v-model="disabled" title="Disabled" />
+    </template>
+  </Story>
+</template>
+
+<style lang="scss" scoped>
+.playground {
+  height: 400px;
+}
+</style>
---
+++ src/components/CcSelect.vue
@@ -0,0 +1,78 @@
+<script setup lang="ts">
+import { ref } from "vue";
+import CcIcon from "./CcIcon.vue";
+
+const inputValue = ref("");
+const isOpen = ref(false);
+
+const toggleMenu = (val?: boolean) => {
+  isOpen.value = val === undefined ? !isOpen.value : val;
+};
+</script>
+
+<template>
+  <div>
+    <div ref="selectEl" class="cc-select" :class="{ 'cc-select--open': isOpen }">
+      <input class="cc-select__input" v-model="inputValue" />
+      <button class="cc-select__toggle" @click="toggleMenu()">
+        <CcIcon name="chevron-down" />
+      </button>
+      <ul v-if="isOpen" class="cc-option">
+        <li>Tom</li>
+        <li>Jane</li>
+        <li>Peter</li>
+        <li>Mary</li>
+        <li>Jordan</li>
+        <li>Polly</li>
+        <li>Amanda</li>
+        <li>Billy</li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.cc-select {
+  position: relative;
+
+  display: flex;
+  align-items: center;
+
+  padding: 4px 8px;
+
+  border: 1px solid black;
+  border-radius: $border-radius-sm;
+
+  &__input {
+    width: 100%;
+  }
+
+  &__toggle {
+    display: grid;
+    place-content: center;
+
+    background-color: transparent;
+
+    svg {
+      transition: transform 0.2s ease;
+    }
+  }
+
+  &--open {
+    .cc-select__toggle svg {
+      transform: rotate(180deg);
+    }
+  }
+}
+
+.cc-option {
+  position: absolute;
+
+  padding: 4px 8px;
+
+  border: 1px solid black;
+  border-radius: $border-radius-sm;
+
+  background-color: #fff;
+}
+</style>

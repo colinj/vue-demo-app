8909f2635636190ac1f90d84c66391fa9fdbf11f
Colin Johnsun <colin.johnsun@gmail.com>
2022-09-15 19:49:40 +1000

    wip: Inital commit for Tooltip

---
+++ src/components/CcTooltip.story.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import CcTooltip from "./CcTooltip.vue";
+</script>
+
+<template>
+  <Story title="CcTooltip">
+    <Variant title="default">
+      <CcTooltip />
+    </Variant>
+
+    <!-- <template #controls>
+      <HstText v-model="state.name" title="Icon" />
+      <HstSelect v-model="state.kind" :options="iconKind" title="Icon style" />
+      <HstSelect v-model="colorName" :options="colorNames" title="Color" />
+      <HstSelect v-model="colorShade" :options="colorShades" title="Shade" />
+      <HstText v-model="state.size" title="Size" />
+    </template> -->
+  </Story>
+</template>
---
+++ src/components/CcTooltip.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="cc-tooltip">
+    <div>The target word</div>
+    <div>The lengthy tooltip description about this item</div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.cc-tooltip {
+  position: relative;
+}
+</style>

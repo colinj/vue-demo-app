c18bf506e7f79578058508bfe697627b53f9a37f
Colin Johnsun <colin.johnsun@gmail.com>
2022-09-19 15:02:14 +1000

    feat: Pagination component

---
+++ src/components/CcPagination.story.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import { reactive } from "vue";
+import CcPagination from "./CcPagination.vue";
+
+const state = reactive({
+  page: 1,
+  pageSize: 10,
+  totalItems: 35,
+});
+</script>
+
+<template>
+  <Story title="CcPagination">
+    <Variant title="default">
+      <CcPagination
+        v-model="state.page"
+        v-model:page-size="state.pageSize"
+        :total-items="state.totalItems"
+      ></CcPagination>
+    </Variant>
+
+    <template #controls>
+      <HstNumber v-model="state.page" title="Page" />
+      <HstNumber v-model="state.pageSize" title="Page size" />
+      <HstNumber v-model="state.totalItems" title="Total items" />
+    </template>
+  </Story>
+</template>
---
+++ src/components/CcPagination.vue
@@ -0,0 +1,164 @@
+<script setup lang="ts">
+import { computed, ref, watch } from "vue";
+import CcIcon from "./CcIcon.vue";
+import CcDropdown from "./CcDropdown.vue";
+
+interface Props {
+  modelValue: number;
+  totalItems: number;
+  pageSize: number;
+  hidePageSize?: boolean;
+}
+const props = withDefaults(defineProps<Props>(), {
+  hidePageSize: false,
+});
+const emit = defineEmits<{
+  (e: "update:modelValue", value: number): void;
+  (e: "update:pageSize", value: number): void;
+}>();
+
+const getRange = (min: number, max: number) => [...Array(max - min + 1).keys()].map((i) => min + i);
+
+const totalPages = computed(() => Math.ceil(props.totalItems / props.pageSize));
+const range = computed(() => {
+  return totalPages.value < 5
+    ? { min: 1, max: totalPages.value }
+    : props.modelValue < 4
+    ? { min: 1, max: 5 }
+    : props.modelValue > totalPages.value - 3
+    ? { min: totalPages.value - 4, max: totalPages.value }
+    : { min: props.modelValue - 2, max: props.modelValue + 2 };
+});
+const firstItem = computed(() => (props.modelValue - 1) * props.pageSize + 1);
+const lastItem = computed(() => Math.min(props.modelValue * props.pageSize, props.totalItems));
+const isActive = (val: number) => (val === props.modelValue ? "cc-pagination--active" : null);
+const setPage = (val: number) => {
+  console.log("new page", val);
+  emit("update:modelValue", val);
+};
+const pageSizeModel = ref(props.pageSize);
+watch(pageSizeModel, (val, oldVal) => {
+  const oldFirst = (props.modelValue - 1) * oldVal + 1;
+  emit("update:modelValue", Math.ceil(oldFirst / val));
+  emit("update:pageSize", val);
+});
+</script>
+
+<template>
+  <div v-if="props.totalItems > 0" class="cc-pagination">
+    <div data-test-id="show-total-items" class="cc-pagination__description">
+      <span v-if="hidePageSize">
+        showing {{ firstItem }} to {{ lastItem }} of {{ props.totalItems }} result{{
+          props.totalItems === 1 ? "" : "s"
+        }}
+      </span>
+      <CcDropdown v-else v-model="pageSizeModel" :options="[10, 20, 40, 60]">
+        <span>
+          showing {{ firstItem }} to {{ lastItem }} of {{ props.totalItems }} result{{
+            props.totalItems === 1 ? "" : "s"
+          }}
+        </span>
+      </CcDropdown>
+    </div>
+    <div class="cc-pagination__control">
+      <div data-test-id="arrow-previous-page" class="cc-pagination__part">
+        <button class="cc-pagination__item" :disabled="props.modelValue === 1" @click="setPage(props.modelValue - 1)">
+          <CcIcon name="chevron-left" />
+        </button>
+        <button v-if="range.min > 1" class="cc-pagination__item" @click="setPage(1)">1</button>
+      </div>
+      <div v-if="range.min > 2" class="cc-pagination__sep">&hellip;</div>
+      <div data-test-id="pagination-numbers" class="cc-pagination__part">
+        <button
+          v-for="num in getRange(range.min, range.max)"
+          :key="num"
+          class="cc-pagination__item"
+          :class="isActive(num)"
+          @click="setPage(num)"
+        >
+          {{ num }}
+        </button>
+      </div>
+      <div v-if="range.max < totalPages - 1" class="cc-pagination__sep">&hellip;</div>
+      <div data-test-id="arro`w-`next-page" class="cc-pagination__part">
+        <button v-if="range.max < totalPages" class="cc-pagination__item" @click="setPage(totalPages)">
+          {{ totalPages }}
+        </button>
+        <button
+          class="cc-pagination__item"
+          :disabled="props.modelValue === totalPages"
+          @click="setPage(props.modelValue + 1)"
+        >
+          <CcIcon name="chevron-right" />
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.cc-pagination {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+
+  &__control {
+    display: inline-flex;
+    justify-content: flex-end;
+
+    width: 380px;
+  }
+
+  &__part {
+    display: flex;
+
+    & + & {
+      margin-left: 16px;
+    }
+  }
+
+  &__sep {
+    flex-grow: 1;
+
+    text-align: center;
+  }
+
+  &__item {
+    display: inline-flex;
+    justify-content: center;
+    align-items: center;
+
+    width: 24px;
+    height: 24px;
+    padding: 0;
+
+    border: 0;
+    border-radius: 50%;
+
+    background-color: transparent;
+
+    &:hover:not(.cc-pagination--active, :disabled) {
+      background-color: color(grey-100);
+    }
+
+    /* stylelint-disable-next-line no-descending-specificity */
+    & + & {
+      margin-left: 16px;
+    }
+  }
+
+  &--active {
+    background-color: color(grey-200);
+  }
+
+  @media (max-width: 640px) {
+    justify-content: center;
+
+    &__control {
+      justify-content: center;
+
+      margin-top: 24px;
+    }
+  }
+}
+</style>

e6f9b555df842905c8e2bfb7ade45fe8cb0daaaf
Colin Johnsun <colin.johnsun@gmail.com>
2022-10-03 13:02:40 +1100

    refactor: Move existing pages into new layout

--- src/modules/Home/HomeView.vue
+++ src/modules/Home/HomeView.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import TheWelcome from "@/components/TheWelcome.vue";
+import HelloWorld from "@/components/HelloWorld.vue";
 </script>
 
 <template>
-  <main>
+  <main class="p-10">
+    <HelloWorld class="pb-10" msg="Welcome to the home page!" />
     <TheWelcome />
   </main>
 </template>

878569c7a414c4c69a6ae21c08f6b91f0774e1e1
Colin Johnsun <colin.johnsun@gmail.com>
2022-10-03 18:26:04 +1100

    fix: Checkbox can be single value modelValue

--- src/components/CcCheckbox.story.vue
+++ src/components/CcCheckbox.story.vue
@@ -5,13 +5,17 @@ import CcCheckbox from "./CcCheckbox.vue";
 const name1 = ref<string[]>([]);
 const name2 = ref<string[]>([]);
 const name3 = ref<string[]>([]);
+const counters = ref<number[]>([]);
+const toggle = ref(false);
+const person = ref("");
+const counter = ref(0);
 const disabled = ref(false);
 const indeterminate = ref(false);
 </script>
 
 <template>
   <Story title="CcCheckbox" :layout="{ type: 'grid', width: '100%' }">
-    <Variant title="Options array">
+    <Variant title="Options string array">
       <CcCheckbox
         v-model="name1"
         :options="['Tom', 'Peter', 'Mary']"
@@ -19,9 +23,20 @@ const indeterminate = ref(false);
         :indeterminate="indeterminate"
         class="grid"
       />
-
       <div>{{ name1 }}</div>
     </Variant>
+
+    <Variant title="Options number array">
+      <CcCheckbox
+        v-model="counters"
+        :options="[23, 15, 99, 47, 62, 91]"
+        :disabled="disabled"
+        :indeterminate="indeterminate"
+        class="grid"
+      />
+      <div>{{ counters }}</div>
+    </Variant>
+
     <Variant title="Options object">
       <CcCheckbox
         v-model="name2"
@@ -29,16 +44,30 @@ const indeterminate = ref(false);
         :disabled="disabled"
         class="grid"
       />
-
       <div>{{ name2 }}</div>
     </Variant>
 
     <Variant title="Multiple checkboxes">
       <CcCheckbox v-model="name3" value="John" :disabled="disabled" class="grid" />
       <CcCheckbox v-model="name3" value="Peter" :disabled="disabled" class="grid" />
-
       <div>{{ name3 }}</div>
     </Variant>
+
+    <Variant title="Boolean checkbox">
+      <CcCheckbox v-model="toggle" :value="true" :disabled="disabled" class="grid">Feature X</CcCheckbox>
+      <div>{{ toggle }}</div>
+    </Variant>
+
+    <Variant title="String value checkbox">
+      <CcCheckbox v-model="person" value="Peter" :disabled="disabled" class="grid">This is Peter</CcCheckbox>
+      <div>{{ person }}</div>
+    </Variant>
+
+    <Variant title="Number value checkbox">
+      <CcCheckbox v-model="counter" :value="123" :disabled="disabled" class="grid">This is valued at 123</CcCheckbox>
+      <div>{{ counter }}</div>
+    </Variant>
+
     <template #controls>
       <HstCheckbox v-model="disabled" title="Disabled" />
       <HstCheckbox v-model="indeterminate" title="Indeterminate" />
--- src/components/CcCheckbox.vue
+++ src/components/CcCheckbox.vue
@@ -7,7 +7,7 @@ type CheckboxOptions = Array<CheckboxValue> | Record<string, CheckboxValue>;
 
 interface Props {
   id?: string;
-  modelValue: CheckboxValue[];
+  modelValue: CheckboxValue | CheckboxValue[];
   options?: CheckboxOptions;
   value?: CheckboxValue;
   indeterminate?: boolean;
@@ -16,6 +16,7 @@ interface Props {
 const props = withDefaults(defineProps<Props>(), {
   indeterminate: false,
   disabled: false,
+  value: true,
 });
 const emit = defineEmits<{
   (e: "update:modelValue", v: CheckboxValue | CheckboxValue[] | undefined): void;
@@ -33,10 +34,21 @@ const checkboxOptions = computed<[string, CheckboxValue][]>(() => {
 
 const onChange = (e: Event) => {
   const target = e.target as HTMLInputElement;
-  const selected = target.checked
-    ? [...props.modelValue, target.value]
-    : props.modelValue.filter((x) => x !== target.value);
-  emit("update:modelValue", selected);
+  if (Array.isArray(props.modelValue)) {
+    const selected = target.checked
+      ? [...props.modelValue, target.value]
+      : props.modelValue.filter((x) => x !== target.value);
+    emit("update:modelValue", selected);
+  } else {
+    const selected = target.checked
+      ? props.value
+      : typeof props.value === "string"
+      ? ""
+      : typeof props.value === "number"
+      ? 0
+      : false;
+    emit("update:modelValue", selected);
+  }
 };
 
 watch(
@@ -47,12 +59,12 @@ watch(
 
 <template>
   <div :class="{ 'cc-checkbox--disabled': props.disabled }" @change="onChange">
-    <div v-for="(option, idx) in checkboxOptions" :key="option[0]" class="cc-checkbox">
+    <div v-for="([value, caption], idx) in checkboxOptions" :key="value" class="cc-checkbox">
       <input
         :id="`${cbId}--${idx}`"
-        :checked="inputValue.includes(option[0])"
+        :checked="inputValue.includes(value)"
         type="checkbox"
-        :value="option[0]"
+        :value="value"
         :indeterminate="props.indeterminate"
         :disabled="props.disabled"
       />
@@ -60,7 +72,7 @@ watch(
         <svg class="cc-checkbox__check" viewBox="0 0 12 10">
           <polyline points="1 6 5 9 11 1" />
         </svg>
-        <slot>{{ option[1] }}</slot>
+        <slot>{{ caption }}</slot>
       </label>
     </div>
   </div>

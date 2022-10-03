33bc506502ac1c91ee7dde918dd632a8497adec7
Colin Johnsun <colin.johnsun@gmail.com>
2022-10-03 20:29:25 +1100

    feat: Add entity DTO types

---
+++ src/types/Id.ts
@@ -0,0 +1,6 @@
+export type Id<T> = number & { __type?: T };
+
+export type UserId = Id<"user">;
+export type PostId = Id<"post">;
+export type CommentId = Id<"comment">;
+export type TodoId = Id<"todo">;
